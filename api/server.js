dbee7938205f76619865ce71d6972871cb70c563
amlane <twelvexstring@gmail.com>
2019-10-20 21:38:02 -0500

    items can be gotten, posted, putted and deleted

--- api/server.js
+++ api/server.js
@@ -4,15 +4,17 @@ const cors = require("cors");
 
 const authRouter = require("../auth/auth-router.js");
 const usersRouter = require("../users/users-router.js");
+const itemsRouter = require("../items/items-router.js");
 
 const server = express();
 
 server.use(helmet());
 server.use(express.json());
-server.use(cors());
+server.use(cors({ origin: '*' }))
 
 server.use("/api/auth", authRouter);
 server.use("/api/users", usersRouter);
+server.use("/api/items", itemsRouter);
 
 server.get("/", (req, res) => {
     res.send("🔥🔥🔥 It's alive! 🔥🔥🔥");
